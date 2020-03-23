1542a8a06a32f4fd000b0e5eed70d3fe4efed3fb
automatica-cluj <60926519+automatica-cluj@users.noreply.github.com>
2020-03-24 00:43:12 +0200

    Initial commit

---
+++ src/main/java/isp/lab5/exercise1/Exercise1.java
@@ -0,0 +1,10 @@
+package isp.lab5.exercise1;
+
+//import isp.lab4.exercise0.CarAlarm; //NU ASA
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        //instantiati obiect
+    }
+}
